4dd6911b9b5f7cb6804c0fe6430c6caefea10c9e
guimartinsalmeida <guimartinsalmeida@gmail.com>
2019-09-18 21:00:07 -0500

    view login

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,41 @@
 const router = require('express').Router()
 const passport = require('../handlers/passport')
 const ensureLogin = require('connect-ensure-login')
-const {catchErrors} = require('../middlewares/catchErrors')
+const { catchErrors } = require('../middlewares/catchErrors')
 const checkRole = require('../middlewares/checkRole')
-const {mail} = require('../middlewares/sendMail')
-const {login, loginForm, logout, profile, staffprofile, studentprofile} = require('../controllers/index.controller')
+const { mail } = require('../middlewares/sendEmail')
+const { login, loginForm, logout, profile, staffprofile, studentprofile } = require('../controllers/index.controller')
 
-const {createUser, createUserForm, deleteUser} = require('../controllers/bosscontroller')
-const {editStaffForm, editStaff, createStudentForm, createStudent, deleteStudent, inviteGuestForm, inviteGuest, deleteGuest} = require('../controllers/staffcontroller')
-const {createGuestForm, createGuest} = require('../controllers/studentscontroller')
+const { createUser, createUserForm, deleteUser } = require('../controllers/bosscontroller')
+const {
+  editStaffForm,
+  editStaff,
+  createStudentForm,
+  createStudent,
+  deleteStudent,
+  inviteGuestForm,
+  inviteGuest,
+  deleteGuest
+} = require('../controllers/staffcontroller')
+const { createGuestForm, createGuest } = require('../controllers/studentscontroller')
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
 router.get('/login', loginForm)
 router.post('/login', passport.authenticate('local'), login)
 //Boss routes
 router.get('/profile', checkRole('BOSS'), ensureLogin.ensureLoggedIn(), profile)
-router.get('/create-staff',  ensureLogin.ensureLoggedIn(), checkRole('BOSS'), createUserForm)
-router.post('/create-staff',  ensureLogin.ensureLoggedIn(), checkRole('BOSS'), catchErrors(createUser))
+router.get('/create-staff', ensureLogin.ensureLoggedIn(), checkRole('BOSS'), createUserForm)
+router.post('/create-staff', ensureLogin.ensureLoggedIn(), checkRole('BOSS'), catchErrors(createUser))
 router.get('/delete-user/:id', ensureLogin.ensureLoggedIn(), checkRole('BOSS'), catchErrors(deleteUser))
 
 //Staff routes
 router.get('/staffprofile', checkRole('STAFF'), ensureLogin.ensureLoggedIn(), staffprofile)
 router.get('/edit-staff', checkRole('STAFF'), ensureLogin.ensureLoggedIn(), editStaffForm)
 router.post('/edit-staff', checkRole('STAFF'), ensureLogin.ensureLoggedIn(), catchErrors(editStaff))
-router.get('/create-student', checkRole('STAFF'), ensureLogin.ensureLoggedIn(), createStudentForm )
+router.get('/create-student', checkRole('STAFF'), ensureLogin.ensureLoggedIn(), createStudentForm)
 router.post('/create-student', checkRole('STAFF'), ensureLogin.ensureLoggedIn(), catchErrors(createStudent))
 router.get('/invite-guest', ensureLogin.ensureLoggedIn(), checkRole('STAFF'), inviteGuestForm)
 router.post('/invite-guest', ensureLogin.ensureLoggedIn(), checkRole('STAFF'), mail, catchErrors(inviteGuest))
@@ -35,4 +44,4 @@ router.get('/delete-student/:id', ensureLogin.ensureLoggedIn(), checkRole('STAFF
 //Student routes
 router.get('/studentprofile', checkRole('STUDENT'), ensureLogin.ensureLoggedIn(), studentprofile)
 router.get('/logout', logout)
-module.exports = router;
+module.exports = router
