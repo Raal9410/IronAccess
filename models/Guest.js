f6cc3ba54580a2ef43bc6d5fd2d1539a7b56875c
guimartinsalmeida <guimartinsalmeida@gmail.com>
2019-09-18 20:10:11 -0500

    models

---
+++ models/Guest.js
@@ -0,0 +1,27 @@
+const { model, Schema } = require('mongoose')
+
+const guestSchema = new Schema(
+  {
+    name: String,
+    email: String,
+    lastName: String,
+    date: {
+      type: Date,
+      default: Date.now(),
+      required: true
+    },
+    code: {
+      type: String,
+      unique: true
+    },
+    invitedBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = model('Guest', guestSchema)
